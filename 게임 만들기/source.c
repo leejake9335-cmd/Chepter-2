4482c7012d2f1566b618f75b411f3cd434dca2b4
leejake9335-cmd <leejake9335@gmail.com>
2026-04-23 19:39:41 +0900

    (2026.04.23) 소리 시스템

---
+++ 게임 만들기/source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+struct 
+{
+	int hp;
+	int attack;
+	int defense;
+	int speed;
+	int exp;
+	int level;
+	int mental;
+	int Introversion;
+
+};
+
+
+int main
